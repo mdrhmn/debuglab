e81023d644501112664277fbb7c881c4f088c237
mdrhmn <mdrhmn99@gmail.com>
2023-11-07 01:53:07 +0800

    feat: added layout, push notification and loading indicator

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <BasePushNotification />
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+const client = useSupabaseClient();
+const authStore = useAuthStore();
+
+const { data: { session } } = await client.auth.getSession();
+authStore.$patch({ session: session });
+
+if (!session && authStore.isLoggedIn) {
+  const { data, error } = await client.auth.refreshSession()
+  const { session, user } = data
+  authStore.$patch({ session: session });
+}
+</script>
+
+<style></style>
