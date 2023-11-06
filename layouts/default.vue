b00b1539c00224c33ab1b275a57e807158aae4fa
mdrhmn <mdrhmn99@gmail.com>
2023-11-07 01:43:59 +0800

    feat: added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <BaseNavbar />
+        <div class="bg-gray-50 dark:bg-gray-900 flex flex-col min-h-screen justify-center">
+            <slot />
+        </div>
+        <BaseFooter />
+    </div>
+</template>
+
+<style></style>
