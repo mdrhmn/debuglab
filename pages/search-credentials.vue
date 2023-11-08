992a0bb2535383170ac974bb2728114796f32717
mdrhmn <mdrhmn99@gmail.com>
2023-11-08 12:30:07 +0800

    style: added loading spinner

--- pages/search-credentials.vue
+++ pages/search-credentials.vue
@@ -22,8 +22,11 @@ const username = ref({
         .label("Username"),
 });
 
+const loading = ref(false)
+
 // Handle successful form submission
 const onSubmit = async (values: any) => {
+    loading.value = true
     const formResult = values
     const router = useRouter()
     router.push({ path: "/credentials", query: { username: formResult.username } })
@@ -80,7 +83,8 @@ const onInvalidSubmit = ({ values, errors, results }: any) => {
                                 </div>
 
                                 <div class="flex-[0_0_auto]">
-                                    <button type="submit" class=" p-4 inline-flex justify-center items-center gap-2
+
+                                    <button v-if="!loading" type="submit" class=" p-4 inline-flex justify-center items-center gap-2
                                         rounded-md border border-transparent font-semibold bg-orange-500 text-white
                                         hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500
                                         focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
@@ -89,6 +93,15 @@ const onInvalidSubmit = ({ values, errors, results }: any) => {
                                             <path
                                                 d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                                         </svg>
+                                    </button>
+                                    <button v-else type="submit" class=" p-4 inline-flex justify-center items-center gap-2
+                                        rounded-md border border-transparent font-semibold bg-orange-500 text-white
+                                        hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500
+                                        focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
+                                        <div class="animate-spin inline-block w-4 h-4 border-[3px] border-current border-t-transparen rounded-full"
+                                            role="status" aria-label="loading">
+                                            <span class="sr-only">Loading...</span>
+                                        </div>
                                     </button>
                                 </div>
                             </div>
