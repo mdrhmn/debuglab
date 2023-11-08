afb52cce4e43d9de4fdb3936f3bd1769c4ee4f56
mdrhmn <mdrhmn99@gmail.com>
2023-11-08 08:59:39 +0800

    feat: added scroll to top btn

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template>
     <div>
-        <!-- Title -->
-        <!-- <div class="max-w-2xl mx-auto text-center mt-20 lg:mb-10">
-            <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">See Our <span
-                    class="text-orange-500 dark:text-orange-600">Courses</span></h2>
-            <p class="mt-1 text-gray-600 dark:text-gray-400">Whatever your status, our offers evolve according to your
-                needs.</p>
-        </div> -->
-        <!-- End Title -->
-
         <HomeHero />
         <HomeCourses />
         <HomeSchedule />
@@ -16,6 +7,7 @@
         <HomeCoaches />
         <HomePlatforms />
         <HomePDF />
+        <BaseScrollToTop />
     </div>
 </template>
 
